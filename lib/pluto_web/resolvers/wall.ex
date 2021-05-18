432cf02cdc45dad488b224c2fbd85058d2b46ba1
Mike Chen <mike.chen@access-company.com>
2021-05-18 15:03:36 +0800

    Add pagination to list_posts query

--- lib/pluto_web/resolvers/wall.ex
+++ lib/pluto_web/resolvers/wall.ex
@@ -1,7 +1,12 @@
 defmodule PlutoWeb.Resolvers.Wall do
+  import Ecto.Query
+
+  alias Absinthe.Relay.Connection
   alias Pluto.Wall
 
-  def posts(_, _) do
-    {:ok, Wall.list_posts()}
+  def posts(pagination_args, _) do
+    Wall.Post
+    |> order_by(desc: :inserted_at)
+    |> Connection.from_query(&Pluto.Repo.all/1, pagination_args)
   end
 end
