8af767cae7637f1e8ce34cb43e9fea8f8eab9ff0
Mike Chen <mike.chen@access-company.com>
2021-05-19 17:43:11 +0800

    Adjust style

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -8,8 +8,7 @@ import { makeStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles({
   root: {
-    width: '100%',
-    height: '100%',
+    padding: '1rem',
   },
 })
 
--- frontend/src/wall/CreatePostBox.tsx
+++ frontend/src/wall/CreatePostBox.tsx
@@ -4,22 +4,22 @@ import { styled } from '@material-ui/core/styles'
 const BoxContainer = styled(Box)({
   backgroundColor: 'rgba(193,200,212, 0.1)',
   marginBottom: '0.4rem',
-  padding: '1rem',
-  height: 200,
+  padding: '0 0.8rem',
+  height: '13rem',
+  borderRadius: '0.3rem',
 })
 
 const StyledTextArea = styled(TextField)({ flex: 1, height: '100%' })
-type Props = {}
 
-function CreatePostBox(props: Props) {
+function CreatePostBox() {
   return (
     <BoxContainer display="flex" flexDirection="column">
       <Box flex={1} display="flex">
         <StyledTextArea
           multiline={true}
-          rows={6}
+          rows={5}
           placeholder="Write something"
-          InputProps={{ disableUnderline: true, style: { flex: 1 } }}
+          InputProps={{ disableUnderline: true, style: { flex: 1, fontSize: '1.5rem' } }}
         />
       </Box>
       <Box display="flex" flexDirection="row-reverse">
