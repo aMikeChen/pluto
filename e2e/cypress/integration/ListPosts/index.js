95656cf67bc3c912595fd3e7c3921e10e6a78177
oLeviChiang <levi.chiang@access-company.com>
2021-06-02 15:14:32 +0800

    Add ListPost feature

--- e2e/cypress/integration/ListPosts/index.js
+++ e2e/cypress/integration/ListPosts/index.js
@@ -1,4 +1,4 @@
-import { Given, Then } from 'cypress-cucumber-preprocessor/steps'
+import { Given, Then, When } from 'cypress-cucumber-preprocessor/steps'
 
 Given('{int} posts in the system', (n) => {
   cy.insertList('post', n)
@@ -10,4 +10,8 @@ Given('I am on the home page', () => {
 
 Then('I see {int} posts', (n) => {
   cy.get('[data-testid=postCard]').should('have.length', n)
+})
+
+When('I scroll to the {string}', (position) => {
+  cy.scrollTo(position)
 })
