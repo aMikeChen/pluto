c89629922c1c64194f570f5e117af4a1febb4917
oStevenChiu <steven.chiu@access-company.com>
2021-06-02 16:26:24 +0800

    Refactor instruction and function

--- e2e/cypress/integration/ListPosts/index.js
+++ e2e/cypress/integration/ListPosts/index.js
@@ -1,13 +1,9 @@
-import { Given, Then, When } from 'cypress-cucumber-preprocessor/steps'
+import { Given, Then } from 'cypress-cucumber-preprocessor/steps'
 
 Given('{int} posts in the system', (n) => {
   cy.insertList('post', n)
 })
 
 Then('I see {int} posts', (n) => {
-  cy.get('[data-testid=postCard]').should('have.length', n)
-})
-
-When('I scroll to the {string}', (position) => {
-  cy.scrollTo(position)
+  cy.the('postCard').should('have.length', n)
 })
