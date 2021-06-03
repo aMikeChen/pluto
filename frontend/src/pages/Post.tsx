48bf829587c57ace5dc635dc6c51e46eb65c0e79
oLeviChiang <levi.chiang@access-company.com>
2021-06-03 17:00:57 +0800

    [pt-48] Add post UI

--- frontend/src/pages/Post.tsx
+++ frontend/src/pages/Post.tsx
@@ -1,13 +1,41 @@
 import { useParams } from 'react-router-dom'
+import { Card, CardContent, Typography } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import { getTimeAgo } from '../app/time'
 
 interface ParamTypes {
   id: string
 }
 
+const useStyles = makeStyles({
+  card: {
+    background: '#40516E',
+    marginBottom: '0.8rem',
+    contentVisibility: 'auto',
+  },
+  timestamp: {
+    color: '#AAAAAA',
+  },
+})
+
 function Post() {
   const { id } = useParams<ParamTypes>()
+  const classes = useStyles()
 
-  return <div>{id}</div>
+  return (
+    <div>
+      <Card className={classes.card}>
+        <CardContent>
+          <Typography variant="body1" color="primary">
+            {id}
+          </Typography>
+          <Typography variant="subtitle2" className={classes.timestamp}>
+            {getTimeAgo(new Date('2020-10-10T00:00:00'))}
+          </Typography>
+        </CardContent>
+      </Card>
+    </div>
+  )
 }
 
 export default Post
