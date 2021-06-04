3ec2f9483ed825584c238fb3b6af96325a2131d6
oStevenChiu <steven.chiu@access-company.com>
2021-06-04 15:40:31 +0800

    Query post

--- frontend/src/pages/Post.tsx
+++ frontend/src/pages/Post.tsx
@@ -1,9 +1,49 @@
-import Content from '../wall/Content'
+import graphql from 'babel-plugin-relay/macro'
+import { loadQuery, usePreloadedQuery } from 'react-relay'
+import { useParams } from 'react-router-dom'
+import RelayEnvironment from '../relay/RelayEnvironment'
+import { PostQuery as PostQueryType } from './__generated__/PostQuery.graphql'
+import PostContent from '../wall/PostContent'
+import { makeStyles, Typography } from '@material-ui/core'
+
+const PostQuery = graphql`
+  query PostQuery($id: ID!) {
+    post(id: $id) {
+      ...PostContent_post
+    }
+  }
+`
+
+const useStyles = makeStyles({
+  root: {
+    padding: '1rem',
+  },
+})
+
+interface ParamTypes {
+  id: string
+}
+
+const preloadedQuery = (id: string) => loadQuery<PostQueryType>(RelayEnvironment, PostQuery, { id })
 
 function Post() {
+  const classes = useStyles()
+  const { id } = useParams<ParamTypes>()
+  const data = usePreloadedQuery(PostQuery, preloadedQuery(id))
+
+  if (!data.post) {
+    return (
+      <div>
+        <Typography variant="h1" color="primary">
+          Post Not Found
+        </Typography>
+      </div>
+    )
+  }
+
   return (
-    <div>
-      <Content />
+    <div className={classes.root}>
+      <PostContent post={data.post} />
     </div>
   )
 }
